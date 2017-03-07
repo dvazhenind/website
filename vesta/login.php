0df90e8f3292e1384597754244cefc100c35e9ad
dvazhenind <dvazhenind@gmail.com>
2017-03-07 15:59:44 +0600

    Version 2

--- vesta/login.php
+++ vesta/login.php
@@ -257,10 +257,6 @@ if(isset($_POST['btn-login']))
 						</div>
 					</div>
 					<div class="col-sm-3">
-						<div class="address">
-							<img src="images/home/map.png" alt="" />
-							<p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
-						</div>
 					</div>
 				</div>
 			</div>
